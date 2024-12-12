44cbf7032430c10dbbb8ad56ad9b1f2ab2c2230d
Ripunk <84296947+R1punk@users.noreply.github.com>
2024-12-12 11:01:00 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -4,7 +4,7 @@ apt install libjansson build-essential clang binutils -y
 cp /data/data/com.termux/files/usr/include/linux/sysctl.h /data/data/com.termux/files/usr/include/sys
 cd .. && git clone https://github.com/Darktron/ccminer.git && cd ccminer && chmod +x build.sh configure.sh autogen.sh start.sh && CXX=clang++ CC=clang ./build.sh && rm config.json
 
-echo -e """
+echo -e '''
 {
     "pools":
         [{
@@ -47,5 +47,5 @@ echo -e """
     "retry-pause": 10,
     "api-allow": "192.168.0.0/16",
     "api-bind": "0.0.0.0:4068"
-}""" >> config.json
+}''' >> config.json
 mv config.json /data/data/com.termux/files/home/ccminer/
